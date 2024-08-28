1a63c28b121ecbe77cd1c510b5355e2ed5c90700
Igor Hey Matos <iheymatos1@gmail.com>
2024-08-28 09:27:16 -0300

    Exercicios

---
+++ ListaExerc/Exe06b/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int tamanho = 10;
+int[] valores = new int[tamanho];
+
+Random random = new Random();
+
+for (int i = 0; i < valores.Length; i++)
+{
+    valores[i] = random.Next(10);
+    Console.Write(valores[i] + " | ");
+}
+
+Array.Sort(valores);
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < valores.Length; i++)
+{
+    Console.Write(valores[i] + " | ");
+}
